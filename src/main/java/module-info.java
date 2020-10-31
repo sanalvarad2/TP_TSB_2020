2ce36fef72d9ff9453f9dd55420f897ab6c64ee6
Santiago Alvarado <s.alvarado0@icloud.com>
2020-10-31 13:04:45 -0300

    Add FX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+module TP_TSB_2020{
+        requires javafx.fxml;
+        requires javafx.controls;
+        requires javafx.graphics;
+        opens sample;
+
+}
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,4 +1,9 @@
+package sample;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,8 +14,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/main.fxml"));
         primaryStage.setTitle("Primera ventana");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
 
     }
 }
