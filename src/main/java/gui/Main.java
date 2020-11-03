5a86fe8de9f5f115e0bafc3293839f945b2ba42d
Santiago Alvarado <s.alvarado0@icloud.com>
2020-11-03 20:04:39 -0300

    Cambios

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -6,22 +6,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private static Scene scene;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/main.fxml"));
-        primaryStage.setTitle("Primera ventana");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(loadFXML("main"));
+        stage.setScene(scene);
+        stage.show();
 
 
     }
-    // Hola, Santi.
-    // Juan se la come.
-    //Facu careta
+    static void setRoot(String fxml) throws IOException{
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 }
