e449e78313d4eb2178b46c77afb8ae762e8953c5
Santiago Alvarado <s.alvarado0@icloud.com>
2020-11-06 20:45:48 -0300

    agregado resultados

---
+++ src/main/java/Negocio/Resultados.java
@@ -0,0 +1,12 @@
+package Negocio;
+
+import oahashtable.TSB_OAHashtable;
+
+public class Resultados {
+    private TSB_OAHashtable tablaResultados;
+
+    public Resultados(String path) {
+        tablaResultados =new TSB_OAHashtable();
+
+    }
+}
